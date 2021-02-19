7d652891b31f8e03b5958fa994df1ecde85870c6
a.karzhavin <a.karzhavin@npocompo.ru>
2021-02-19 23:13:33 +0300

    refactoring

--- tests/test_lesson2.py
+++ tests/test_lesson2.py
@@ -8,11 +8,12 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
+  
 
 def test_untitled(driver):
     find_string = 'test'
     driver.get("https://www.google.com/")
-    driver.set_window_size(1440, 877)
+    driver.maximize_window()
     driver.find_element(By.NAME, "q").send_keys(find_string)
     driver.find_element(By.NAME, "q").send_keys(Keys.ENTER)
     WebDriverWait(driver, 10).until(EC.title_is(f"{find_string} - Поиск в Google"))
