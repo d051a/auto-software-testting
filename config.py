84952674513d49b36e00cd755bddb06a9a509648
Каржавин Александр <a.karzhavin@npocompo.ru>
2021-02-27 16:31:37 +0300

    refactoring

---
+++ config.py
@@ -0,0 +1,3 @@
+LITECART_BASE_URL = 'http://192.168.56.200/litecart'
+USER_LOGIN = 'admin@mail.ru'
+USER_PASSWORD = 'password'
--- tests/lesson2/test_login_lesson2.py
+++ tests/lesson2/test_login_lesson2.py
@@ -7,15 +7,15 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from config import LITECART_BASE_URL, USER_LOGIN, USER_PASSWORD
+
 
   
-LITECART_BASE_URL = 'http://192.168.56.200/litecart'
+# LITECART_BASE_URL = 'http://192.168.56.200/litecart'
 
 def test_login(driver):
-    user_login = 'admin@mail.ru'
-    user_password = 'password'
     driver.get(LITECART_BASE_URL)
     driver.maximize_window()
-    driver.find_element(By.NAME, "email").send_keys(user_login)
-    driver.find_element(By.NAME, "password").send_keys(user_password)
+    driver.find_element(By.NAME, "email").send_keys(USER_LOGIN)
+    driver.find_element(By.NAME, "password").send_keys(USER_PASSWORD)
     driver.find_element(By.XPATH, "//button[@name='login']").send_keys(Keys.ENTER)
