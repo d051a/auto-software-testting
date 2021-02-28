1005a56a0493dc366f07646af543c56d9c1c3413
Каржавин Александр <a.karzhavin@npocompo.ru>
2021-02-28 11:28:27 +0300

    refactoring

--- tests/lesson2/test_lesson2.py
+++ tests/lesson2/test_lesson2.py
@@ -1,14 +1,8 @@
-import pytest
-import time
-import json
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
-  
 
 def test_untitled(driver):
     find_string = 'test'
--- tests/lesson2/test_login_lesson2.py
+++ tests/lesson2/test_login_lesson2.py
@@ -1,17 +1,7 @@
-import pytest
-import time
-import json
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from config import LITECART_BASE_URL, USER_LOGIN, USER_PASSWORD
 
-
-  
-# LITECART_BASE_URL = 'http://192.168.56.200/litecart'
 
 def test_login(driver):
     driver.get(LITECART_BASE_URL)
