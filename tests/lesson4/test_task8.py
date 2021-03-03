b78a7668dd781931ccda8203196d57e9ace7a444
Каржавин Александр <a.karzhavin@npocompo.ru>
2021-03-03 19:32:45 +0300

    task8 solution correction

--- tests/lesson4/test_task8.py
+++ tests/lesson4/test_task8.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 
 def test_good_item_sticker_exist(driver, login_customer):
     goods_category1_link_locator = "//li[@class='category-1']"
-    goods_list_locator = "//li[@class='product column shadow hover-light']"
+    goods_list_locator = "//li[contains(@class,'product')]"
     sticker_locator = ".//div[contains(@class, 'sticker')]"
 
     goods_category1_link = driver.find_element(By.XPATH, goods_category1_link_locator)
